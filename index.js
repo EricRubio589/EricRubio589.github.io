fd7e69742a4424ca1fa8f5dc3a0c97a2fd345ac3
Eric Rubio <eric.rubio.services@gmail.com>
2019-04-23 22:39:30 -0500

    changes made to the functions

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+///We define the global variable that will hold the question number///
+
+var questionIndex = 0;
 
 //When the start button is clicked we remove the initial section to start the quiz and //
 //we show the section containing the questions//
 function showQuestionsScreen() {
-    $('.beginQuiz').on('click', '.beginButton', function beginButtonClicked(event) {
+    $('.beginQuiz').on('click', '#beginButton', function beginButtonClicked(event) {
         console.log("beginButton clicked");
-    event.preventDefault();    
+    event.preventDefault();   
     $('.beginQuiz').remove();
-    $('.questionsContainer')
+    $('.questionAndAnswersContainer')
         .show()
-        .append(questionTemplate(questionsAnswers[0]));
+        .append(questionTemplate(questionsAnswers[questionIndex]));
 });
 }
 
@@ -16,46 +19,105 @@ function showQuestionsScreen() {
 const questionsAnswers = [
     {
         toRespond:'this is the question',
-        answer1:'first answer',
-        answer2:'second answer',
-        answer3:'third answer',
-        answer4:'fourth answer'
+        ans:{
+            one:'first answer',
+            two:'second answer',
+            three:'third answer',
+            four:'fourth answer',
+            correct:2
+        }
     }
 ] 
 
 ///this is the html inserted for the mobile version of the quiz///
 function questionTemplate(question) {
     return `
-    <div id="questionForm">
-        <span id="questionText">${question.toRespond}</span>
-            <form>
-                <fieldset>
-                    <label>
+    <div class="questionFormContainer">
+        <div class="questionBox">
+            <span class="questionText">${question.toRespond}</span>
+        </div>    
+            <form class="questionForm">
+                <fieldset class="answersContainer">
+                    <label class="answerBox">
                         <input class="answers" type="radio" name="answer" required>
-                        <span>${question.answer1}</span>
+                        <span>${question.ans.one}</span>
                     </label>
-                    <label>
+                    <label class="answerBox">
                             <input class="answers" type="radio" name="answer" required>
-                            <span>${question.answer2}</span>
+                            <span>${question.ans.two}</span>
                     </label>
-                    <label>
+                    <label class="answerBox">
                             <input class="answers" type="radio" name="answer" required>
-                            <span>${question.answer3}</span>
+                            <span>${question.ans.three}</span>
                     </label>
-                    <label>
+                    <label class="answerBox">
                             <input class="answers" type="radio" name="answer" required>
-                            <span>${question.answer4}</span>
+                            <span>${question.ans.four}</span>
                     </label>
-                    <button id="submitAnswer">Submit</button>
-                    </fieldset>
+                    <button type="submit" class="submitAnswerButton">Submit</button>
+
+                    <div class="scoreAndQuestionNumberDisplay">
+                        <span>Question 1/10</span>
+                        <span>Score 0/10</span>
+                    </div>
+                </fieldset>
             </form>
     </div>
 `;
 }
 
+////this function will handle the HTML inserted for the wrong answer feedback////
+function wrongAnswerTemplate() {
+    return `
+    <div id="wrongFeedbackContainer">
+        <span id="wrongFeedbackText">Wrong answer</span>
+        <img id="wrongFeedbackImage" src="https://media.giphy.com/media/iZCd5DtKEiMq4/giphy.gif" alt="tumbling Wario in defeat">
+        <button id="wrongFeedbackContinueButton">Continue</button>
+    </div>
+    `
+}
+
+////this function will handle the HTML inserted for the correct answer feedback////
+function correctAnswerTemplate() {
+    return `
+    
+    `
+}
+
+////this function will handle the submit answer button being activated////
+function handleSubmitAnswerButton() {
+    $('#mainContainer').on('click', '.submitAnswerButton', function submitButtonClicked(event) {
+            event.preventDefault();
+            console.log("submit button clicked");
+            $('.questionAndAnswersContainer').css('display','none');
+            /*$(`.feedbackContainer`).show().append(wrongAnswerTemplate);*/
+        });
+}
+
+////this is what we will display in case the answer was wrong////
+function wrongAnswerFeedback() {
+
+}
+
+////this is what we will display in case the answer was correct////
+function correctAnswerFeedback() {
+
+}
+
+////with this we will handle the score after submiting the answer////
+function handleScore() {
+    
+}
+
 function initQuizApp() {
     showQuestionsScreen();
-    questionTemplate(questionsAnswers);
+    questionTemplate();
+    handleSubmitAnswerButton();
+    wrongAnswerFeedback();
+    wrongAnswerTemplate();
+    correctAnswerFeedback();
+    correctAnswerTemplate();
+    handleScore();
 }
 
 $(initQuizApp);
