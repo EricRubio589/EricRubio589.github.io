2bf8ad4abffed4b9531c739e8726114b26cf6ba0
Eric Rubio <eric.rubio.services@gmail.com>
2019-05-07 23:38:08 -0500

    Modified the background and restart functions

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ var score = 0;
 
 //When the start button is clicked we remove the initial section to start the quiz and //
 //we show the section containing the questions//
-function showQuestionsScreen() {
+function handleClickStartButton() {
     $('.beginQuiz').on('click', '#beginButton', function beginButtonClicked(event) {
         console.log("beginButton clicked");
     event.preventDefault();   
-    $('.beginQuiz').hide();
+    startQuiz();
+});
+}
+
+function startQuiz() {
+   $('.beginQuiz').hide();
     $('.questionAndAnswersContainer').show();
     renderQuestionAndAnswers();
-    renderScoreAndQuestionBanner();
-});
+    renderScoreAndQuestionBanner(); 
 }
 
 ///this is the array of objects containing the questions and answers///
@@ -21,97 +25,98 @@ const questionsAnswers = [
     {
         toRespond:`What video game is widely considered the worst video game of all times and partially credited as
          responsible for the video game industry crash in 1983?`,
-        ans:['E.T. the Extra-Terrestrial','Superman','Dr. Jekyll and Mr. Hide','The Cheetahmen',1]
+        answers:['E.T. the Extra-Terrestrial','Superman','Dr. Jekyll and Mr. Hide','The Cheetahmen',1]
     },
 
     {
         toRespond:`In 1991 the first console war took place between which 2 video game consoles?`,
-        ans:['Nintendo Entertainment System and Sega Master System','Playstation and Nintendo 64',
+        answers:['Nintendo Entertainment System and Sega Master System','Playstation and Nintendo 64',
         'Atari 2600 and Mattel Intellivision','Super Nintendo Entertainment System and Sega Genesis',4]
     },
 
     {
         toRespond:`This Iconic character debuted in the arcade game Donkey Kong in 1981 and was originally called Jumpman.`,
-        ans:['Sonic','Donkey Kong','Mario','Link',3]
+        answers:['Sonic','Donkey Kong','Mario','Link',3]
     },
 
     {
         toRespond:`This nonlinear game utilized a “rock paper scissors” mechanic that allowed the player to select which
          stage to play based on previous power-ups obtained granting an easier battle with said’s level final boss.`,
-        ans:['The Legend of Zelda','Megaman','Rock paper scissors','Teenage Mutant Ninja Turtles',2]
+        answers:['The Legend of Zelda','Megaman','Rock paper scissors','Teenage Mutant Ninja Turtles',2]
     },
 
     {
         toRespond:`This 1986 video game combines the platforming of Super Mario Bros. and the adventure of The Legend
          of Zelda with a dark science fiction atmosphere and greater emphasis on nonlinear gameplay.`,
-        ans:['Metroid','DuckTales','Bionic Commando','Castlevania',1]
+        answers:['Metroid','DuckTales','Bionic Commando','Castlevania',1]
     },
 
     {
         toRespond:`IT’S DANGEROUS TO GO ALONE! TAKE THIS” are the famous words said by an old man to this game’s main
          character as he hands him a sword to commence his quest.`,
-        ans:['The Legend Of Zelda','Sword Master','ActRaiser','Prince of Persia',1]
+        answers:['The Legend Of Zelda','Sword Master','ActRaiser','Prince of Persia',1]
     },
 
     {
         toRespond:`In this game the main character (Simon Belmont) utilizes a magic whip as his primary (but not only)
          weapon to defeat Count Dracula and other mythical creatures.`,
-        ans:[`Bram Stoker's Dracula`,'Castlevania','Monster Party','ActRaiser',2]
+        answers:[`Bram Stoker's Dracula`,'Castlevania','Monster Party','ActRaiser',2]
     },
 
     {
         toRespond:`Bill Rizer and Lance Bean, are sent to the Galuga archipelago to destroy the enemy forces and uncover
          the true nature of Red Falcon, the alien entity controlling them in this game that utilizes a variety of playing 
          perspectives.`,
-        ans:['Bionic Commando','Final Fantasy',`Ghosts 'n Goblins`,'Contra',4]
+        answers:['Bionic Commando','Final Fantasy',`Ghosts 'n Goblins`,'Contra',4]
     },
 
     {
         toRespond:`This game is played from a first-person perspective and requires the NES Zapper light gun, which the 
         player aims and fires at the screen. first released in Japan in April 1984, and was released as a launch game 
         for the NES in North America in October 1985.`,
-        ans:['Gumshoe','Laser Invasion','Barker Bills Trick Shooting','Duck Hunt',4]
+        answers:['Gumshoe','Laser Invasion','Barker Bills Trick Shooting','Duck Hunt',4]
     },
 
     {
         toRespond:`Combat in this game is menu-based: the player selects an action from a list of options such as Attack, 
         Magic, and Item. Battles are turn-based and continue until either side flees or is defeated. If the player's party 
         wins, each character will gain experience and Gil (The game’s currency).`,
-        ans:['Final Fantasy','Dungeons and Dragons','EarthBound','Dragon Warrior',1]
+        answers:['Final Fantasy','Dungeons and Dragons','EarthBound','Dragon Warrior',1]
     }
 ]
 
 ///Here we render the questions ///
 function renderQuestionAndAnswers() {
-    $('.questionAndAnswersContainer').html(questionTemplate(questionsAnswers[questionIndex]));
+    $('.questionAndAnswersContainer').html(questionTemplate(questionsAnswers[questionIndex]));  
 }
 
 ///this is the html inserted for the mobile version of the quiz///
 function questionTemplate(question) {
     return `
     <div class="questionFormContainer">
-        <div class="questionBox">
-            <span class="questionText">${question.toRespond}</span>
-        </div>    
-            <form class="questionForm">
+        <form class="questionForm">
+            <div class="questionBox">
+                <span class="questionText">${question.toRespond}</span>
+            </div>    
                 <fieldset class="answersContainer">
                     <label class="answerBox">
                         <input class="answers" type="radio" name="answer" required>
-                        <span>${question.ans[0]}</span>
+                        <span>${question.answers[0]}</span>
                     </label>
                     <label class="answerBox">
-                            <input class="answers" type="radio" name="answer" required>
-                            <span>${question.ans[1]}</span>
+                        <input class="answers" type="radio" name="answer" required>
+                        <span>${question.answers[1]}</span>
                     </label>
                     <label class="answerBox">
-                            <input class="answers" type="radio" name="answer" required>
-                            <span>${question.ans[2]}</span>
+                        <input class="answers" type="radio" name="answer" required>
+                        <span>${question.answers[2]}</span>
                     </label>
                     <label class="answerBox">
-                            <input class="answers" type="radio" name="answer" required>
-                            <span>${question.ans[3]}</span>
+                        <input class="answers" type="radio" name="answer" required>
+                        <span>${question.answers[3]}</span>
                     </label>
-                    <button type="submit" class="submitAnswerButton">Submit</button>
+
+                     <button type="submit" class="submitAnswerButton">Submit</button>
 
                     <div class="scoreAndQuestionNumberDisplay">
                         <span class="displayQuestionInBody">Question ${questionIndex+1}/10</span>
@@ -125,8 +130,8 @@ function questionTemplate(question) {
 
 ////this function will handle the HTML inserted for the wrong answer feedback////
 function wrongAnswerTemplate() {
-    var answerIndex = questionsAnswers[questionIndex-1].ans[4];
-    var correctAnswerText = questionsAnswers[questionIndex-1].ans[answerIndex-1];
+    var answerIndex = questionsAnswers[questionIndex-1].answers[4];
+    var correctAnswerText = questionsAnswers[questionIndex-1].answers[answerIndex-1];
     return `
     <div id="wrongFeedbackContainer">
         <form id="wrongFeedbackForm">
@@ -182,7 +187,7 @@ function badFinalScoreTemplate() {
 ////this is the html template for the score and question number to be inserted in the banner////
 function scoreAndQuestionBannerTemplate() {
     return `<span class="displayQuestionInBanner">Question ${questionIndex+1}/10</span>
-    <span class="displayScoreInBanner">Score ${score}/10</span>`
+    <span id="displayScoreInBanner">Score ${score}/10</span>`
 }
 
 ////this function will handle the submit answer button being activated////
@@ -195,15 +200,13 @@ function handleSubmitAnswerButton() {
         checkIfCorrectOrWrong(submittedAnswer, questionsAnswers[questionIndex]);
         if (questionIndex < 10) {
         renderScoreAndQuestionBanner();
-        } else {
-            $('.scoreAndQuestionBannerDisplay').hide();
         }
-        });
+    });
 }
 
 ////here we will check if the answer is correct////
 function checkIfCorrectOrWrong(answer, compareValues) {
-    if (answer + 1 === compareValues.ans[4]) {
+    if (answer + 1 === compareValues.answers[4]) {
         score ++;
         questionIndex ++;
         correctAnswerFeedback();
@@ -220,14 +223,22 @@ function handleNextButton() {
         event.preventDefault();
         console.log("Button Next Clicked");
         if (questionIndex < 10) {
-        renderQuestionAndAnswers();
-        $('.feedbackContainer').hide();
-        $('.questionAndAnswersContainer').css('display','flex');
+            nextQuestion();
         } else {
             handleFinalScore();
             $(`.scoreAndQuestionBannerDisplay`).hide();
-        }  
+        }    
     });
+}
+
+function nextQuestion() {
+    renderQuestionAndAnswers();
+    $('.feedbackContainer').hide();
+    $('.questionAndAnswersContainer').css('display','flex');
+    renderScoreAndQuestionBanner();
+    /*if (window.innerWidth > 1023) {*/ 
+       /* $('.scoreAndQuestionBannerDisplay').show(); */
+    /*}*/
 }
 
 ///Here we will render the score and quesiton number in the banner ///
@@ -251,15 +262,31 @@ function handleFinalScore() {
     if (score > 5) {
         $('.feedbackContainer').show().html(goodFinalScoreTemplate);
     } else {
-        $('.feedbackContainer').show().html(badFinalScoreTemplate)
+        $('.feedbackContainer').show().html(badFinalScoreTemplate);     
     }
+}
+
+function handleRestartButton() {
+    $(`.feedbackContainer`).on(`submit`, `form`, function restartButtonClicked(event) {
+        console.log('restart button clicked');
+        event.preventDefault();
+        questionIndex = 0;
+        score = 0;
+        nextQuestion();
+        if (window.innerWidth > 1023) {
+            $('.scoreAndQuestionBannerDisplay').show();
+         }
+    });
 }
 
 ////finally we call the functions that we need on start up////
 function initQuizApp() {
-    showQuestionsScreen();
+    handleClickStartButton();
     handleSubmitAnswerButton();
     handleNextButton();
+    handleRestartButton();
+    
+    
 }
 
 $(initQuizApp);
